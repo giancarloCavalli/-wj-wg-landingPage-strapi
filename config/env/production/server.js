1549421690961716bb6d31403da46bf0926a91ca
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-02-19 11:00:28 -0300

    chore: config strapi production keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env('MY_HEROKU_URL'),
   proxy: true,
   app: {
-    keys: env.array('APP_KEYS', 'STRAPI_KEY1', 'STRAPI_KEY2'),
+    keys: env.array('APP_KEYS', ['STRAPI_KEY1', 'STRAPI_KEY2']),
   },
 });
